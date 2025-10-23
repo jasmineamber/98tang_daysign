e6183f9fe054fa0d732a9fb2ac00655468bd3039
jasmineamber <jasmineamber@aliyun.com>
2025-10-23 22:22:09 +0800

    增加等待时间

--- daysign.py
+++ daysign.py
@@ -280,3 +280,5 @@ if __name__ == "__main__":
     for i in range(3):
         index = i + 1
         main(index)
+        sleep_time = random.uniform(60 * 2, 60 * 5)
+        time.sleep(sleep_time)
